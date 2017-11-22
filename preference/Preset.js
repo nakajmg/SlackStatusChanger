0761d95058ad8d4f662446b42bab5e9d32dfefdc
nakajmg <nakajima.jmg@gmail.com>
2017-11-22 17:59:40 +0900

    presetの追加削除ができるように

--- preference/Preset.js
+++ preference/Preset.js
@@ -12,20 +12,23 @@ module.exports = {
         <span>Preset</span>
       </label>
       
-      <div class="PresetItem" v-for="(item, index) in preset">
-        <div class="control has-icons-left has-icons-right">
-          <input class="input is-medium PresetItem__Text" :value="item.status_text" @input="updateText($event, index)">
-          <span @click="showPicker(index)" class="icon is-left PresetItem__Emoji">
-            <Emoji :emoji="item.status_emoji" :set="emojiSet" :backgroundImageFn="emojiSheet" :size="24"/>
-          </span>
-          <span class="icon is-right PresetItem__Remove">
-            <Emoji emoji=":x:" :set="emojiSet" :size="16" :backgroundImageFn="emojiSheet"/>
-          </span>
+      <div class="PresetItems">
+        <div class="PresetItem" v-for="(item, index) in preset">
+          <div class="control has-icons-left has-icons-right">
+            <input class="input is-medium PresetItem__Text" :value="item.status_text" @input="updateText($event, index)">
+            <span @click="showPicker(index)" class="icon is-left PresetItem__Emoji">
+              <Emoji :emoji="item.status_emoji" :set="emojiSet" :backgroundImageFn="emojiSheet" :size="24"/>
+            </span>
+            <span class="icon is-right PresetItem__Remove" @click="removePresetItem(index)">
+              <Emoji emoji=":x:" :set="emojiSet" :size="16" :backgroundImageFn="emojiSheet"/>
+            </span>
+          </div>
         </div>
       </div>
       
-      <div v-if="false" class="control is-clearfix" style="text-align: center;">
-        <span class="icon is-large">
+      
+      <div class="control is-clearfix" style="text-align: center;">
+        <span class="icon is-large" @click="addPresetItem">
           <Emoji class="PresetItem__Add" emoji=":heavy_plus_sign:" :set="emojiSet" :backgroundImageFn="emojiSheet"  :size="24"/>
         </span>
       </div>
@@ -84,6 +87,19 @@ module.exports = {
       this.preset[this.selectedIndex].status_emoji = status_emoji
       this.update(this.preset)
       this.selectedIndex = null
+    },
+
+    addPresetItem() {
+      this.preset.push({
+        status_emoji: ':smiley:',
+        status_text: ''
+      })
+      this.update(this.preset)
+    },
+
+    removePresetItem(index) {
+      this.preset.splice(index, 1)
+      this.update(this.preset)
     }
   },
 
