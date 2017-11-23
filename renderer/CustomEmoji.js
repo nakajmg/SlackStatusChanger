ce7ac91e3262aa96894e9ce7ba834c8b2631cdff
nakajmg <nakajima.jmg@gmail.com>
2017-11-23 15:34:21 +0900

    ショートカットキー設定

--- renderer/CustomEmoji.js
+++ renderer/CustomEmoji.js
@@ -4,7 +4,7 @@ const {Emoji} = require('emoji-mart-vue')
 module.exports = {
   template: `
   <span v-if="item.custom" class="emoji-mart-emoji emoji">
-    <img :src="imageUrl(item.status_emoji)" alt="" width="24" height="24" style="display: block;">
+    <img :src="imageUrl(item.status_emoji)" alt="" width="20" height="20" style="display: block;">
   </span>
   <Emoji v-else :size="20" :emoji="item.status_emoji" :set="emojiSet" :backgroundImageFn="emojiSheet"/>
   `,
@@ -13,10 +13,10 @@ module.exports = {
   ],
   methods: {
     imageUrl(status_emoji) {
-      if (!status_emoji || this.customEmojis.length === 0) return ''
+      if (!status_emoji || !this.customEmojis || this.customEmojis.length === 0) return ''
       const name = status_emoji.substring(1, status_emoji.length - 1)
       const emoji = find(this.customEmojis, {name})
-      return emoji.imageUrl
+      return emoji ? emoji.imageUrl : ''
     }
   },
   components: {
--- renderer/StatusList.js
+++ renderer/StatusList.js
@@ -1,6 +1,8 @@
 const {mapActions, mapState} = require('vuex')
 const types = require('../store/types')
 const assign = require('object-assign')
+const Mousetrap = require('mousetrap')
+const {each} = require('lodash')
 
 module.exports = {
   template: `
@@ -17,10 +19,33 @@ module.exports = {
     items: 'preset',
     customEmojis: state => state.team.customEmojis
   }),
+
+  created() {
+    this.bindShortcut()
+  },
+
+  updated() {
+    this.unbindShortcut()
+    this.bindShortcut()
+  },
+
   methods: assign(mapActions({
     setStatus: types.SET_CURRENT_STATUS,
     openPreference: types.OPEN_PREFERENCE,
   }), {
+    bindShortcut() {
+      each(this.items, (item, index) => {
+        if (index >= 9) return
+        Mousetrap.bind(`command+${index + 1}`, () => {
+          this.setStatus(item)
+        })
+      })
+    },
+    unbindShortcut() {
+      for(let i = 1; i < 10; i++) {
+        Mousetrap.unbind(`command+${i}`)
+      }
+    }
   }),
   components: {
   }
